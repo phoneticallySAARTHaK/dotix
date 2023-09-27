acecd39b89a253354832cc1ea8971526190318e6
Sarthak Batham <sarthakbatham26@gmail.com>
2023-09-27 15:30:59 +0530

    redcirect from root

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,13 @@
+import { LoaderFunctionArgs, redirect } from "react-router-dom";
 import { api } from "../api";
 
 export const Component = () => {
   return null;
 };
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
   !api.getToken() && (await api.fetchToken());
-  return null;
+  const pathname = new URL(request.url).pathname;
+  if (pathname !== "/") return null;
+  else redirect("/home");
 }
